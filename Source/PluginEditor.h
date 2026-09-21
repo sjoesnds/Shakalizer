c33c7433da0a4c44935170d486482332a4e62009
sjoesnds <centralizedboss@gmail.com>
2026-09-21 19:52:31 +0300

    feat: full v1 editor and modulation matrix

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -47,22 +47,24 @@ private:
         float level = 0.0f;
     };
 
-    static constexpr int sliderCount = 31;
+    static constexpr int sliderCount = 36;
 
     void configureSlider(juce::Slider&, const juce::String&,
                          double min, double max, double step);
     void addAttachment(const juce::String&, juce::Slider&);
 
     void timerCallback() override;
+
     void saveA();
     void swapAB();
     void toggleAutoMatch();
+    void toggleSmart();
 
     void randomizeAll();
     void randomizeScope(int scope);
+
     void loadPreset(int index);
 
-    void updateAutoMatchButton();
     void captureState(std::vector<float>& destination);
     void applyState(const std::vector<float>& state);
 
@@ -80,10 +82,33 @@ private:
     juce::ComboBox qualityBox;
     juce::ComboBox syncBox;
     juce::ComboBox glitchGridBox;
+    juce::ComboBox routingBox;
+    juce::ComboBox msModeBox;
+    juce::ComboBox liveSceneBox;
+
+    std::array<juce::ComboBox*, 4> modSourceBoxes {
+        nullptr, nullptr, nullptr, nullptr
+    };
+
+    std::array<juce::ComboBox*, 4> modDestBoxes {
+        nullptr, nullptr, nullptr, nullptr
+    };
+
+    juce::ComboBox modSource1Box;
+    juce::ComboBox modSource2Box;
+    juce::ComboBox modSource3Box;
+    juce::ComboBox modSource4Box;
+
+    juce::ComboBox modDest1Box;
+    juce::ComboBox modDest2Box;
+    juce::ComboBox modDest3Box;
+    juce::ComboBox modDest4Box;
 
     juce::TextButton saveAButton { "SAVE A" };
     juce::TextButton abButton { "A / B" };
     juce::TextButton autoMatchButton { "AUTO" };
+    juce::TextButton smartButton { "SMART" };
+
     juce::TextButton randomAllButton { "RANDOM ALL" };
     juce::TextButton randomCoreButton { "CORE" };
     juce::TextButton randomShatterButton { "SHATTER" };
@@ -125,6 +150,12 @@ private:
     juce::Slider preGainSlider;
     juce::Slider smoothSlider;
 
+    juce::Slider mod1AmountSlider;
+    juce::Slider mod2AmountSlider;
+    juce::Slider mod3AmountSlider;
+    juce::Slider mod4AmountSlider;
+    juce::Slider morphSlider;
+
     std::array<juce::Slider*, sliderCount> sliders {
         &shakalSlider, &destroySlider, &crushSlider, &decimateSlider,
         &driveSlider, &clipSlider, &glitchSlider, &jitterSlider,
@@ -132,24 +163,76 @@ private:
         &movementSlider, &unstableSlider, &alienSlider,
         &filterFreqSlider, &filterResSlider, &mixSlider, &outputSlider,
         &shatterSlider, &foldSlider, &shiftSlider, &resonanceSlider,
-        &envFollowSlider, &bandLowSlider, &bandMidSlider, &bandHighSlider,
-        &bandAirSlider, &characterSlider, &preGainSlider, &smoothSlider
+        &envFollowSlider, &bandLowSlider, &bandMidSlider,
+        &bandHighSlider, &bandAirSlider, &characterSlider,
+        &preGainSlider, &smoothSlider,
+        &mod1AmountSlider, &mod2AmountSlider,
+        &mod3AmountSlider, &mod4AmountSlider, &morphSlider
     };
 
     std::array<juce::String, sliderCount> sliderNames {
         "SHAKAL", "DESTROY", "CRUSH", "DECIMATE",
         "DRIVE", "CLIP", "GLITCH", "JITTER",
         "SPLIT", "TRANSIENT", "BODY", "STEREO",
-        "MOVEMENT", "UNSTABLE", "ALIEN",
-        "FILTER FREQ", "RESONANCE", "MIX", "OUTPUT",
-        "SHATTER", "FOLD", "SHIFT", "RESONATOR",
-        "ENVELOPE", "LOW SHATTER", "MID SHATTER",
-        "HIGH SHATTER", "AIR SHATTER", "CHARACTER",
-        "PRE GAIN", "SMOOTH"
+        "MOVEMENT", "UNSTABLE", "ALIEN", "FILTER FREQ",
+        "RESONANCE", "MIX", "OUTPUT", "SHATTER",
+        "FOLD", "SHIFT", "RESONATOR", "ENVELOPE",
+        "LOW SHATTER", "MID SHATTER", "HIGH SHATTER",
+        "AIR SHATTER", "CHARACTER", "PRE GAIN", "SMOOTH",
+        "MOD 1", "MOD 2", "MOD 3", "MOD 4", "MORPH"
     };
 
     std::vector<std::unique_ptr<
-        juce::AudioProcessorValueTreeState::SliderAttachment>> attachments;
+        juce::AudioProcessorValueTreeState::SliderAttachment>>
+        attachments;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        modeAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        resampleAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        filterAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        movementAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        qualityAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        syncAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        glitchGridAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        routingAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        msModeAttachment;
+
+    std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>
+        liveSceneAttachment;
+
+    std::array<std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>, 4>
+        modSourceAttachments;
+
+    std::array<std::unique_ptr<
+        juce::AudioProcessorValueTreeState::ComboBoxAttachment>, 4>
+        modDestAttachments;
 
     std::vector<float> abState;
     bool hasAState = false;
