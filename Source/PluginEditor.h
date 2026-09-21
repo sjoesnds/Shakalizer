ac41e0b276ca560acda51ef5792a0a9876220e5c
sjoesnds <centralizedboss@gmail.com>
2026-09-21 14:30:41 +0300

    feat: expose all Shakalizer v0.3 controls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,24 +28,32 @@ private:
         void drawComboBox(juce::Graphics&, int, int, bool, int, int, int, int,
                           juce::ComboBox&) override;
 
-        void drawButtonBackground(juce::Graphics&, juce::Button&, const juce::Colour&, bool, bool) override;
+        void drawButtonBackground(juce::Graphics&, juce::Button&,
+                                  const juce::Colour&, bool, bool) override;
     };
 
     class Meter final : public juce::Component
     {
     public:
-        void setLevel(float v) { level = juce::jlimit(0.0f, 1.0f, v); repaint(); }
+        void setLevel(float v)
+        {
+            level = juce::jlimit(0.0f, 1.0f, v);
+            repaint();
+        }
+
         void paint(juce::Graphics&) override;
+
     private:
         float level = 0.0f;
     };
 
-    static constexpr int sliderCount = 18;
+    static constexpr int sliderCount = 30;
 
-    void configureSlider(juce::Slider&, const juce::String&, double min, double max, double step);
+    void configureSlider(juce::Slider&, const juce::String&,
+                         double min, double max, double step);
     void addAttachment(const juce::String&, juce::Slider&);
-    void timerCallback() override;
 
+    void timerCallback() override;
     void saveA();
     void swapAB();
     void toggleAutoMatch();
@@ -60,19 +68,21 @@ private:
 
     juce::Label titleLabel;
     juce::Label subtitleLabel;
-    juce::Label modeLabel;
 
     juce::ComboBox modeBox;
     juce::ComboBox resampleBox;
     juce::ComboBox filterBox;
     juce::ComboBox movementBox;
+    juce::ComboBox qualityBox;
+    juce::ComboBox syncBox;
+    juce::ComboBox glitchGridBox;
 
     juce::TextButton saveAButton { "SAVE A" };
     juce::TextButton abButton { "A / B" };
     juce::TextButton autoMatchButton { "AUTO MATCH" };
-    juce::TextButton randomButton { "RANDOM" };
+    juce::TextButton randomButton { "RANDOMIZE ALL" };
 
-    juce::Label meterLabel { {}, "OUTPUT" };
+    juce::Label meterLabel { {}, "OUT" };
     Meter meter;
 
     juce::Slider shakalSlider;
@@ -95,20 +105,35 @@ private:
     juce::Slider mixSlider;
     juce::Slider outputSlider;
 
-    std::array<juce::Slider*, sliderCount + 1> sliders {
-        &shakalSlider, &destroySlider, &crushSlider, &decimateSlider,
-        &driveSlider, &clipSlider, &glitchSlider, &jitterSlider,
-        &splitSlider, &transientSlider, &bodySlider, &stereoSlider,
-        &movementSlider, &unstableSlider, &alienSlider,
-        &filterFreqSlider, &filterResSlider, &mixSlider, &outputSlider
+    juce::Slider shatterSlider;
+    juce::Slider foldSlider;
+    juce::Slider shiftSlider;
+    juce::Slider resonanceSlider;
+    juce::Slider envFollowSlider;
+    juce::Slider bandLowSlider;
+    juce::Slider bandMidSlider;
+    juce::Slider bandHighSlider;
+    juce::Slider bandAirSlider;
+    juce::Slider characterSlider;
+    juce::Slider preGainSlider;
+
+    std::array<juce::Slider*, sliderCount> sliders {
+        &shakalSlider, &destroySlider, &crushSlider, &decimateSlider, &driveSlider,
+        &clipSlider, &glitchSlider, &jitterSlider, &splitSlider, &transientSlider,
+        &bodySlider, &stereoSlider, &movementSlider, &unstableSlider, &alienSlider,
+        &filterFreqSlider, &filterResSlider, &mixSlider, &outputSlider,
+        &shatterSlider, &foldSlider, &shiftSlider, &resonanceSlider, &envFollowSlider,
+        &bandLowSlider, &bandMidSlider, &bandHighSlider, &bandAirSlider,
+        &characterSlider, &preGainSlider
     };
 
-    std::array<juce::String, sliderCount + 1> sliderNames {
-        "SHAKAL", "DESTROY", "CRUSH", "DECIMATE",
-        "DRIVE", "CLIP", "GLITCH", "JITTER",
-        "SPLIT", "TRANSIENT", "BODY", "STEREO",
-        "MOVEMENT", "UNSTABLE", "ALIEN",
-        "FILTER FREQ", "RESONANCE", "MIX", "OUTPUT"
+    std::array<juce::String, sliderCount> sliderNames {
+        "SHAKAL", "DESTROY", "CRUSH", "DECIMATE", "DRIVE", "CLIP",
+        "GLITCH", "JITTER", "SPLIT", "TRANSIENT", "BODY", "STEREO",
+        "MOVEMENT", "UNSTABLE", "ALIEN", "FILTER FREQ", "RESONANCE",
+        "MIX", "OUTPUT", "SHATTER", "FOLD", "SHIFT", "RESONATOR",
+        "ENVELOPE", "LOW SHATTER", "MID SHATTER", "HIGH SHATTER",
+        "AIR SHATTER", "CHARACTER", "PRE GAIN"
     };
 
     std::vector<std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>> attachments;
@@ -120,6 +145,9 @@ private:
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> resampleAttachment;
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> filterAttachment;
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> movementAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> qualityAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> syncAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> glitchGridAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ShakalizerAudioProcessorEditor)
 };
